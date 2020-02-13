3bc96dddeb5c3fd054155573a371077ae8c1b474
AnnKemp <ann.kemp@scarlet.be>
2020-02-13 11:10:35 +0100

    raw start

--- game.php
+++ game.php
@@ -1,1 +1,31 @@
 <?php
+session_start();
+
+// Set session variables
+$_SESSION["player"] = 0;
+$_SESSION["house"] = 0;
+echo "Session variables are set.";
+
+class Blackjack{
+
+    $score=0;
+
+    function Hit(){
+        $this score+=$lay_card=mt_rand(1,11);
+    }
+
+    function Stand(){
+        $_SESSION["player"] = 0;
+        echo "Session variables are set.";
+    }
+
+    function Surrender(){
+        echo "The house has won!";
+        // show total dealer
+    }
+}
+
+$player=new Blackjack();
+$dealer=new Blackjack();
+?>
+
